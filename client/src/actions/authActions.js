f6bb364a8a337efaacb2a2ca8704306b03e21435
Dawid <siema@gmail.com>
2019-04-08 21:29:13 +0200

    Fixing

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,11 +2,7 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import {
-  GET_ERRORS,
-  SET_CURRENT_USER,
-  USER_LOADING
-} from "./types";
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -28,7 +24,7 @@ export const loginUser = userData => dispatch => {
     .then(res => {
       // Save to localStorage
 
-// Set token to localStorage
+      // Set token to localStorage
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       // Set token to Auth header
