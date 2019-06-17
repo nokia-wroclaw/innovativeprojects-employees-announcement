a3b705546325596be5df3a1f223288c10185df0a
Dawid <siema@gmail.com>
2019-06-17 20:46:21 +0200

    heroku first

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //mongoURI: "mongodb://localhost:27017/announcements",
+  mongoURI:
+    "mongodb+srv://admin1:admin1@cluster0-uuotn.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/announcements",
-  //mongolab URI -> "mongodb+srv://admin1:admin1@cluster0-uuotn.mongodb.net/test?retryWrites=true&w=majority"
-  secretOrKey: "secret"
-};
